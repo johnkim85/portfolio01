aaf190a7d3802e2443b88cb92339ec3070df8f86
sedeveloper <104178759+johnkim85@users.noreply.github.com>
2022-04-25 16:37:30 +0900

    public class ExceptionCase {
    ExceptionCase

---
+++ ExceptionCase.java
@@ -0,0 +1,16 @@
+package chapter18;
+
+import java.util.Scanner;
+
+public class ExceptionCase {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb=new Scanner(System.in);
+		System.out.print("a/b...a? ");
+		int n1=kb.nextInt();
+		System.out.print("a/b...b? ");
+		int n2=kb.nextInt();
+		System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);
+		System.out.println("Good bye~~!");
+		}
+}
