864b6215c8d21bae13e111f37cd6a12eab74a8e0
sedeveloper <104178759+johnkim85@users.noreply.github.com>
2022-04-25 14:57:04 +0900

    public class PrinterDriver {
    Oh, I did!

---
+++ PrinterDriver.java
@@ -0,0 +1,29 @@
+package personalPractice;
+
+interface Printable{
+	public void print(String doc);
+}
+class SprinterDriver implements Printable{
+	@Override
+	public void print(String doc) {
+		System.out.println("From Samsung printer");
+		System.out.println(doc);
+	}
+}
+class LPrinterDriver implements Printable{
+	@Override
+	public void print(String doc) {
+		System.out.println("From LG printer");
+		System.out.println(doc);
+	}
+}
+public class PrinterDriver {
+	public static void main(String[] args) {
+		String myDoc="This is a report about...";
+		Printable prn=new SprinterDriver();
+		prn.print(myDoc);
+		System.out.println();
+		prn=new LPrinterDriver();
+		prn.print(myDoc);
+	}
+}
