e84d8538c1589ce3eeda38ed9747f96c1cffcb40
sedeveloper <104178759+johnkim85@users.noreply.github.com>
2022-04-25 17:10:56 +0900

    public class ExceptionCase4 {
    try ~ catch here!!

---
+++ ExceptionCase4.java
@@ -0,0 +1,22 @@
+package chapter18;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class ExceptionCase4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb=new Scanner(System.in);
+		try {
+			System.out.print("a/b...a? ");
+			int n1=kb.nextInt();
+			System.out.print("a/b...b? ");
+			int n2=kb.nextInt();
+			System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);
+		}
+		catch(InputMismatchException e) {
+			e.getMessage();
+		}
+		System.out.println("Good bye~~!");
+	}
+}
